6b26c55aebd7c31ea44c15dc60529db1b8df5d37
Dealer_WeARE <wadealer@gmail.com>
2020-08-31 10:54:28 +0300

    don't get 'user' in 'update()' when we don't really need it

--- models/Ion_auth_model.php
+++ models/Ion_auth_model.php
@@ -1797,8 +1797,6 @@ class Ion_auth_model extends CI_Model
 	{
 		$this->trigger_events('pre_update_user');
 
-		$user = $this->user($id)->row();
-
 		$this->db->trans_begin();
 
 		if (array_key_exists($this->identity_column, $data) && $this->identity_check($data[$this->identity_column]) && $user->{$this->identity_column} !== $data[$this->identity_column])
@@ -1821,6 +1819,7 @@ class Ion_auth_model extends CI_Model
 			{
 				if( ! empty($data['password']))
 				{
+					$user = $this->user($id)->row();
 					$data['password'] = $this->hash_password($data['password'], $user->{$this->identity_column});
 					if ($data['password'] === FALSE)
 					{
@@ -1840,7 +1839,7 @@ class Ion_auth_model extends CI_Model
 		}
 
 		$this->trigger_events('extra_where');
-		$this->db->update($this->tables['users'], $data, ['id' => $user->id]);
+		$this->db->update($this->tables['users'], $data, ['id' => $id]);
 
 		if ($this->db->trans_status() === FALSE)
 		{
